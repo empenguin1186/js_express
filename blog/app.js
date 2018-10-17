8743552c33bd3b87c6ec5f651aebf1f0721b7a7c
empenguin1186 <emperorpenguint1186@gmail.com>
2018-10-18 00:18:49 +0900

    add routing method

--- blog/app.js
+++ blog/app.js
@@ -4,6 +4,8 @@ var express = require('express'),
 
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var connect = require('connect');
+var methodOverride = require('method-override');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -14,12 +16,20 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 
 // routing
 
-app.get('/new', function(req, res) {
-    res.render('new');
-});
+app.get('/', post.index);
+
+/*
+app.get('/posts/new', post.new);
+app.post('/posts/create', post.create);
+app.get('/posts/:id', post.show);
+app.get('/posts/:id/edit', post.show);
+app.put('/posts/:id', post.update);
+app.delete('/posts/:id', post.destroy);
+*/
 
 app.listen(3000);
 console.log('server starting ...');
