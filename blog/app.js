c769c6c5af67b12d9bf2709256eed2b458ce8bc4
kodaira <kodaira@yahoo-corp.jp>
2018-10-18 09:17:49 +0900

    add show.ejs

--- blog/app.js
+++ blog/app.js
@@ -21,11 +21,11 @@ app.use(methodOverride('_method'));
 // routing
 
 app.get('/', post.index);
+app.get('/posts/:id', post.show);
 
 /*
 app.get('/posts/new', post.new);
 app.post('/posts/create', post.create);
-app.get('/posts/:id', post.show);
 app.get('/posts/:id/edit', post.show);
 app.put('/posts/:id', post.update);
 app.delete('/posts/:id', post.destroy);
--- blog/routes/post.js
+++ blog/routes/post.js
@@ -1,3 +1,12 @@
+var posts = [
+    {title: 'title01', body: 'body01'},
+    {title: 'title02', body: 'body02'},
+    {title: 'title03', body: 'body03'}
+];
+
 exports.index = function(req, res){
-    res.render('posts/index');
+    res.render('posts/index', {posts: posts});
+};
+exports.show = function(req, res){
+    res.render('posts/show', {post: posts[req.params.id]});
 };
